e0e06c22c3db220848a357ff95684efac298daa6
Erik <ej.erik.jimenez@gmail.com>
2026-02-04 10:46:20 -0600

    lexer function

--- lexer.php
+++ lexer.php
@@ -1,9 +1,31 @@
 <?php
 
-function returnFileBuffer(string $filename, int $maxbuffersize): ?string {
+/**
+ * return an array of parameters of the file given the file name and the max memory that can be used
+ * @return ?string[][]
+ */
+function returnFileBuffer(string $filename, int $maxbuffersize): ?array {
    if(!file_exists($filename)){
       return null;
    }
 
-   fopen($filename, "");
+   $file = fopen($filename, "r");
+
+   $buffer = fread($file, $maxbuffersize);
+   $index = strlen($buffer) - 1;
+
+   //partial entry in buffer
+   while ($buffer[$index] != '\n' && !feof($file)) {
+      $index--;
+   }
+
+   $buffer = substr($buffer, 0, $index);
+
+   $entries = explode('\n', $buffer);
+
+   foreach ($entries as $entry) {
+      $entry = explode(',', $entry);
+   }
+
+   return $entries;
 }
