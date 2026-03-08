c8961869fd9d7b92399127847f5c78978ed3752f
Erik <ej.erik.jimenez@gmail.com>
2026-03-08 15:50:13 -0500

    added spaces to shell_exec statements

--- orchestrator.php
+++ orchestrator.php
@@ -1,12 +1,12 @@
 <?php
 function orchestrate($dataFile, $processCount, $destDirectory, $un, $pw, $host, $db) {
-   shell_exec("safe-split.sh" . $processCount . $dataFile . $destDirectory);
+   shell_exec("safe-split.sh " . $processCount . " " . $dataFile . " " . $destDirectory);
 
    $file_names = scandir($destDirectory);
 
    foreach ($file_names as $file) {
       $filePath = $destDirectory . "/" . $file;
-      shell_exec("run.php" . $un . $pw . $host . $db . $filePath);
+      shell_exec("run.php " . $un . " " . $pw . " " . $host . " " . $db . " " . $filePath. " ");
    }
 }
 ?>
