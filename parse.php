bcbcec6e4783699249912a7b850f99d9ed3f7a23
Erik <ej.erik.jimenez@gmail.com>
2026-03-09 16:39:04 -0500

    insert statements

--- parse.php
+++ parse.php
@@ -22,7 +22,7 @@ function parseTokens(array $tokens, int $numParameters, int &$lineCount, string 
       }
 
       //serial number error
-      $serialNumber = partSerialNumber($entry[2], $prefix, $delimeter, $body);
+      $serialNumberFull = partSerialNumber($entry[2], $prefix, $delimeter, $body);
 
       //incorect size
       if(count($body) != 32) {
@@ -38,24 +38,36 @@ function parseTokens(array $tokens, int $numParameters, int &$lineCount, string 
       if ($errorLine == false && $dbCopy == true) {
          // find if manufacturer is already in the database if not create it
          $sql = "SELECT `manufacturer_id` FROM `manufacturers` WHERE `manufacturer_name` = \"" . $entry[1] . "\";"; 
-         if(!($manufacturerId = $dblink->query($sql))) {
-
+         if(!($manufacturer = $dblink->query($sql)->fetch_column())) {
+            $sql = "INSERT INTO `manufacturers` (`manufacturer_name`) values ('$entry[1]')";
+            $dblink->query($sql);
+            $manufacturerId = $dblink->insert_id();
+         }else {
+            $manufacturerId = $manufacturer['manufacturer_id'];
          }
 
          // find if device type is already in the database if not create it
          $sql = "SELECT `device_type_id` FROM `device_types` WHERE `device_type_name` = \"" . $entry[0] . "\";"; 
-         if(!($deviceTypeId = $dblink->query($sql))) {
-
+         if(!($deviceType = $dblink->query($sql)->fetch_column())) {
+            $sql = "INSERT INTO `device_types` (`device_type_name`) values ('$entry[0]')";
+            $dblink->query($sql);
+            $deviceTypeId = $dblink->insert_id();
+         }else {
+            $deviceTypeId = $deviceType['device_type_id'];
          }
 
          //create new serial number
          $sql = "SELECT `serial_number_id` FROM `serial_numbers` WHERE `prefix` = \"" . $prefix . "\" AND \""  . "`body` = \"" . $body . "\";"; 
-         if(!($serialNumberId = $dblink->query($sql))) {
-
+         if(!($serialNumber = $dblink->query($sql)->fetch_column())) {
+            $sql = "INSERT INTO `serial_numbers` (`serial_number_prefix`, `serial_number_body`) values ('$prefix', '$body')";
+            $dblink->query($sql);
+            $deviceTypeId = $dblink->insert_id();
+         }else {
+            $serialNumberId = $serialNumber['serial_number_id'];
          }
-
+                           
          //insert the entire entry to the main table
-         $sql = "INSERT INTO `devices` (`device_type_id`, `manufacturer_id`, `serial_number_id`, 'line_numer')
+         $sql = "INSERT INTO `devices` (`device_type_id`, `manufacturer_id`, `serial_number_id`, `line_number`)
            values ('$deviceTypeId', '$manufacturerId', '$serialNumberId', '$lineCount')";
          $dblink->query($sql) or die;
 
