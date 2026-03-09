d70d1d4373d1ace6da9dcac8e4781dd932efb46e
Erik <44383316+Mentalish@users.noreply.github.com>
2026-03-09 17:24:27 -0500

    Atomic data

--- parse.php
+++ parse.php
@@ -11,15 +11,69 @@ function parseTokens(array $tokens, int $numParameters, int &$lineCount, string 
             fwrite($logfile, "Missing parameter: " . $i . " at line: " . $lineCount . "\n");
             fclose($logfile);
          }
+
+         //typo
+         
+         //too many tokens in entry
+         if($numParameters != count($entry)) {
+         
+         }
+
       }
 
+      //serial number error
+      $prefix = "";
+      $delimeter = "";
+      $body = "";
+      $serialNumberFull = partSerialNumber($entry[2], $prefix, $delimeter, $body);
+
+      //incorect size
+      if(strlen($body) != 32) {
+      
+      }
+
+      //incorect delimiter
+      if($delimeter != '-') {
+      
+      }
+      
+      //write to db
       if ($errorLine == false && $dbCopy == true) {
-         $sql="Insert Into `devices` (`device_type`, `manufacturer_type`, `serial_number`)
-           values ('$entry[0]', '$entry[1]', '$entry[2]')";
-   $dblink->query($sql) or
-           die("Something went wrong with $sql<br>".$dblink->error); 
+         // find if manufacturer is already in the database if not create it
+         $sql = "SELECT `manufacturer_id` FROM `manufacturers` WHERE `manufacturer_name` = '$entry[1]' ;"; 
+         if(!($manufacturerId = $dblink->query($sql)->fetch_column())) {
+            $sql = "INSERT INTO `manufacturers` (`manufacturer_name`) values ('$entry[1]')";
+            $dblink->query($sql);
+            $manufacturerId = $dblink->insert_id;
+         }
+         // find if device type is already in the database if not create it
+         $sql = "SELECT `device_type_id` FROM `device_types` WHERE `device_type_name` = '$entry[0]' ;"; 
+         if(!($deviceTypeId = $dblink->query($sql)->fetch_column())) {
+            $sql = "INSERT INTO `device_types` (`device_type_name`) values ('$entry[0]')";
+            $dblink->query($sql);
+            $deviceTypeId = $dblink->insert_id;
+         }
+
+         //create new serial number
+         $sql = "SELECT `serial_number_id` FROM `serial_numbers` WHERE `serial_number_prefix` = '$prefix' AND `serial_number_body` = '$body' ;"; 
+         if(!($serialNumberId = $dblink->query($sql)->fetch_column())) {
+            $sql = "INSERT INTO `serial_numbers` (`serial_number_prefix`, `serial_number_body`) values ('$prefix', '$body')";
+            $dblink->query($sql);
+            $serialNumberId = $dblink->insert_id;
+         }                           
+         //insert the entire entry to the main table
+         $sql = "INSERT INTO `devices` (`device_type_id`, `manufacturer_id`, `serial_number_id`, `line_number`)
+           values ('$deviceTypeId', '$manufacturerId', '$serialNumberId', '$lineCount')";
+         $dblink->query($sql) or die;
+
       }  
       $lineCount++;
-   }
+      }
+}
+
+function partSerialNumber(string $fullSerialNumber, &$prefix, &$delimeter, &$body) : void {
+   $prefix = substr($fullSerialNumber, 0,2);
+   $delimeter = $fullSerialNumber[2];
+   $body = substr($fullSerialNumber, 3);
 }
 ?>
