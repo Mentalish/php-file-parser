691c7741e5004c39216178032539d51a0d1201a9
Erik <ej.erik.jimenez@gmail.com>
2026-03-11 13:10:42 -0500

    added line offset for the processes

--- orchestrator.php
+++ orchestrator.php
@@ -3,7 +3,7 @@ function orchestrate($dataFile, $processCount, $destDirectory, $un, $pw, $host, 
    shell_exec("./safe-split.sh " . $processCount . " " . $dataFile . " " . $destDirectory);
 
    $file_names = scandir($destDirectory);
-
+   $lineOffset = 0;
    foreach ($file_names as $file) {
 
       #skip dot directories
@@ -12,7 +12,8 @@ function orchestrate($dataFile, $processCount, $destDirectory, $un, $pw, $host, 
       }
 
       $filePath = $destDirectory . '/' . $file;
-      shell_exec("./run.php " . $un . " " . $pw . " " . $host . " " . $db . " " . $filePath . " > /dev/null 2>&1 &");
+      $lineOffset =+ shell_exec("wc -l " . $filePath);
+      shell_exec("php run.php " . $un . " " . $pw . " " . $host . " " . $db . " " . $filePath . " " . $lineOffset . " > /dev/null 2>&1 &");
    }
 }
 ?>
