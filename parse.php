65e3f528f959ebb5b691cce784ce0e4d5820202b
Erik <ej.erik.jimenez@gmail.com>
2026-03-11 15:56:26 -0500

    fixed regex

--- parse.php
+++ parse.php
@@ -2,7 +2,7 @@
 include 'log.php';
 
 function parseTokens(array $tokens, int $numParameters, int &$lineCount, string $errorLogName, bool $dbCopy, $dblink, &$deviceTypeCache, &$manufacturerCache): void {
-   $typo = '/[0-9@#$%^&*()\'`]|^[a-z]/';
+   $typo = '/(^[a-z])|([0-9@#$%^&*()\'`])/';
    foreach ($tokens as $entry) {
       $errorLine = false;
       for ($i=0; $i < $numParameters; $i++) { 
--- run.php
+++ run.php
@@ -1,6 +1,7 @@
 <?php
-include 'parse.php';
-include 'lexer.php';
+include_once('parse.php');
+include_once('lexer.php');
+include_once('log.php')
 
 $un = $argv[1];
 $pw = $argv[2];
