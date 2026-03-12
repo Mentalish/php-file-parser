e081a584a7a1a67940a8375eba77718fc6e162d6
Erik <44383316+Mentalish@users.noreply.github.com>
2026-03-12 18:07:25 -0500

    refactored parser

--- parse.php
+++ parse.php
@@ -2,73 +2,74 @@
 include_once('log.php');
 
 function parseTokens(array $tokens, int $numParameters, int &$lineCount, string $errorLogName, bool $dbCopy, $dblink, &$deviceTypeCache, &$manufacturerCache): void {
-   $typo = "/(^[a-z])|([0-9@#$%^&*()'`])/";
+   $TYPO_MANUFACTURER = "/(^[a-z])|([0-9@#$%^&*()'`])/";
+   $TYPO_DEVICE_TYPE = "/([0-9@#$%^&*()'`])/";
+   $lineCount++;
    foreach ($tokens as $entry) {
       $errorLine = false;
-      for ($i=0; $i < $numParameters; $i++) { 
-         //empty parameter
-         if($entry[$i] == "") {
-            $errorLine = true;
-            writeToLog($errorLogName, "DATA ERROR", "Missing parameter: " . $i . " at line: " . $lineCount);
-            }
-
-         //typo
-         if($i != 3 && strlen($entry[$i]) == 1 || preg_match($typo, $entry[$i])) {
-            writeToLog($errorLogName, "DATA ERROR", "Typo on parameter: " . $i . " at line: " . $lineCount);
-
-         }
-         
-         //too many tokens in entry
-         if($numParameters != count($entry)) {
-            $errorLine = true;
-            writeToLog($errorLogName, "DATA ERROR", "Too many or too few items in entry");
-         }
-
-      }
-
-      //serial number error
-      $prefix = "";
-      $delimeter = "";
-      $body = "";
-      $serialNumberFull = partSerialNumber($entry[2], $prefix, $delimeter, $body);
-
-      //incorect size
-      if(strlen($body) != 32) {
-         $errorLine = true;
-         writeToLog($errorLogName, "DATA ERROR", "Serial number is the incorrect length on entry number " . $lineCount);
-      }
-
-      //incorect delimiter
-      if($delimeter != '-') {
-         writeToLog($errorLogName, "DATA ERROR (remediated)", "Incorrect delimiter found in serial number on entry number " . $lineCount);
-      }
       
+      //too many tokens in entry
+      if($numParameters != count($entry)) {
+         writeToLog($errorLogName, "DATA ERROR", "Too many or too few items in entry");
+         continue; 
+      }
+
+      $deviceType = $entry[0];
+      $manufacturer = $entry[1];
+      $serialNumber = $entry[2];
+      $prefix = "";
+      $body = "";
+
+      if(detectEmpty($deviceType, "device type", $lineCount, $errorLogName)) {
+         $errorLine = true;
+      } else {
+         if(checkTypo($TYPO_DEVICE_TYPE, $deviceType, "device type", $lineCount, $errorLogName)) {
+         $errorLine = true;
+         }
+      }
+
+      if(detectEmpty($manufacturer, "manufacturer", $lineCount, $errorLogName)) {
+         $errorLine = true;
+      } else {
+         if(checkTypo($TYPO_MANUFACTURER, $manufacturer, "manufacturer", $lineCount, $errorLogName)) {
+            $errorLine = true;
+         }
+      }
+
+      if(detectEmpty($serialNumber, "serial number", $lineCount, $errorLogName)) {
+         $errorLine = true;
+      } else {
+         if(validateSerialNumber($prefix, $body, $serialNumber, $lineCount, $errorLogName)) {
+            $errorLine = true;
+         }
+      } 
+
       //write to db
       if ($errorLine == false && $dbCopy == true) {
          // find if manufacturer is already in the database if not create it
-         if(!isset($manufacturerCache[$entry[1]])) {
-            $sql = "SELECT `manufacturer_id` FROM `manufacturers` WHERE `manufacturer_name` = '$entry[1]' ;"; 
+         if(!isset($manufacturerCache[$manufacturer])) {
+            $sql = "SELECT `manufacturer_id` FROM `manufacturers` WHERE `manufacturer_name` = '$manufacturer' ;"; 
             if(!($manufacturerId = $dblink->query($sql)->fetch_column())) {
-               $sql = "INSERT INTO `manufacturers` (`manufacturer_name`) values ('$entry[1]')";
+               $sql = "INSERT INTO `manufacturers` (`manufacturer_name`) values ('$manufacturer')";
                $dblink->query($sql);
                $manufacturerId = $dblink->insert_id;
             }
-            $manufacturerCache[$entry[1]] = $manufacturerId;
+            $manufacturerCache[$manufacturer] = $manufacturerId;
          } else {
-            $manufacturerId = $manufacturerCache[$entry[1]]; 
+            $manufacturerId = $manufacturerCache[$manufacturer]; 
          }
 
          // find if device type is already in the database if not create it
-         if(!isset($deviceTypeCache[$entry[0]])) {
-            $sql = "SELECT `device_type_id` FROM `device_types` WHERE `device_type_name` = '$entry[0]' ;"; 
+         if(!isset($deviceTypeCache[$deviceType])) {
+            $sql = "SELECT `device_type_id` FROM `device_types` WHERE `device_type_name` = '$deviceType' ;"; 
             if(!($deviceTypeId = $dblink->query($sql)->fetch_column())) {
-               $sql = "INSERT INTO `device_types` (`device_type_name`) values ('$entry[0]')";
+               $sql = "INSERT INTO `device_types` (`device_type_name`) values ('$deviceType')";
                $dblink->query($sql);
                $deviceTypeId = $dblink->insert_id;
             }
-            $deviceTypeCache[$entry[0]] = $deviceTypeId; 
+            $deviceTypeCache[$deviceType] = $deviceTypeId; 
          } else {
-            $deviceTypeId = $deviceTypeCache[$entry[0]];
+            $deviceTypeId = $deviceTypeCache[$deviceType];
          }
          
          //insert the entire entry to the main table
@@ -76,8 +77,7 @@ function parseTokens(array $tokens, int $numParameters, int &$lineCount, string 
            values ('$deviceTypeId', '$manufacturerId', '$prefix', '$body', '$lineCount')";
          $dblink->query($sql) or die;
 
-      }  
-      $lineCount++;
+         }  
       }
 }
 
@@ -89,5 +89,42 @@ function partSerialNumber(string $fullSerialNumber, &$prefix, &$delimeter, &$bod
    $prefix = substr($fullSerialNumber, 0,2);
    $delimeter = $fullSerialNumber[2];
    $body = substr($fullSerialNumber, 3);
+}
+
+function detectEmpty($parameter, $parameterName, $lineNumber, $errorLogName) : bool {
+   if($parameter == "") {
+      writeToLog($errorLogName, "DATA ERROR", "Missing parameter: {" . $parameterName . "} at line: " . $lineNumber);
+      return true;
+   }
+
+   return false;
+}
+
+function checkTypo($typoRegex, $parameter, $parameterName, $lineNumber, $errorLogName) : bool {
+   if(strlen($parameter) == 1 || preg_match ($typoRegex, $parameter)) {
+      writeToLog($errorLogName, "DATA ERROR", "Typo on parameter: " . $parameterName . " at line: " . $lineNumber);
+      return true;
+   }
+
+   return false; 
+}
+
+function validateSerialNumber(&$prefex, &$body, $serialNumber, $lineNumber, $errorLogName) : bool {
+      $delimeter = "";
+      partSerialNumber($serialNumber, $prefix, $delimeter, $body);
+
+      //incorect size
+      if(strlen($body) != 32) {
+         writeToLog($errorLogName, "DATA ERROR", "Serial number is the incorrect length on entry number " . $lineNumber);
+         return true;
+      }
+
+      //incorect delimiter
+      if($delimeter != '-') {
+         writeToLog($errorLogName, "DATA ERROR (remediated)", "Incorrect delimiter found in serial number on entry number " . $lineNumber);
+         return true;
+      }
+
+      return false;
 }
 ?>
