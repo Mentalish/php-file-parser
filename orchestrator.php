58cb4abe9ddfc6bb105ba822fbcbe96964a32035
Erik <44383316+Mentalish@users.noreply.github.com>
2026-03-14 18:45:36 -0500

    improved preformance metrics reporting

--- orchestrator.php
+++ orchestrator.php
@@ -5,7 +5,8 @@ include_once('split.php');
 function orchestrate($dataFile, $processCount, $destDirectory, $un, $pw, $host, $db) {
    $LOGFILE = 'import.log';
    splitFile($dataFile, $destDirectory, intval($processCount), 10);
-   writeToLog($LOGFILE, "START", "Starting import procress on file" . $dataFile);
+   $sourceFileSize = countFile($dataFile);
+   writeToLog($LOGFILE, "START", "Starting import process on file; " . $dataFile . "; " . $sourceFileSize . " records found");
 
    $file_names = scandir($destDirectory);
    $lineOffset = 0;
@@ -17,8 +18,9 @@ function orchestrate($dataFile, $processCount, $destDirectory, $un, $pw, $host, 
       }
 
       $filePath = $destDirectory . '/' . $file;
-      writeToLog($LOGFILE, "PROCESS", "Initializing process number " . ($index - 2)); 
-      shell_exec("php run.php " . $un . " " . $pw . " " . $host . " " . $db . " " . $filePath . " " . $lineOffset . " " . $LOGFILE . " > /dev/null 2>&1 &");
-      $lineOffset += countFile($filePath);  
+      $fileSize = countFile($filePath);
+      writeToLog($LOGFILE, "PROCESS", "Initializing process number " . ($index - 1)); 
+      shell_exec("php run.php " . $un . " " . $pw . " " . $host . " " . $db . " " . $filePath . " " . $lineOffset . " " . $LOGFILE . " " . $fileSize . " > /dev/null 2>&1 &");
+      $lineOffset += $fileSize;  
    }
 }
--- run.php
+++ run.php
@@ -17,6 +17,8 @@ $deviceTypeCache = [];
 
 $file = fopen($fileName, 'r');
 $lineNumber = $argv[6];
+$processNumber = $argv[7];
+$fileEntriesCount = $argv[8];
 while ($tokens = getEntries($file, 10)) {
    parseTokens($tokens, 3, $lineNumber, $argv[7], true, $dblink, $deviceTypeCache, $manufacturerCache);
 }
@@ -25,5 +27,6 @@ $end = microtime(true);
 fclose($file);
 $timeSeconds = $end - $start;
 $timeMin = $timeSeconds/60;
-writeToLog($argv[7], "PROCESS", "Process finished processing file; Time elapsed " . $timeMin);
+$filesPerSec = $fileEntriesCount / $timeSeconds;
+writeToLog($processNumber, "PROCESS", "Process finished processing file; Time elapsed: " . $timeMin . "; Records Processed: " . $fileEntriesCount . "; Files Per Second: " . $filesPerSec);
 ?>
