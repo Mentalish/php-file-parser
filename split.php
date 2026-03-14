5639376e1e1d86a9592bf37bc910313e2aed171f
Erik <44383316+Mentalish@users.noreply.github.com>
2026-03-14 16:47:47 -0500

    native php split

---
+++ split.php
@@ -0,0 +1,49 @@
+<?php
+function splitFile($sourceFile, $destDirectory, $numFiles, $lineBufferSize) {
+   $newFileName = "aaa";
+   $fpointer = fopen($sourceFile, "r");
+   $sourceLineCount = countFile($sourceFile);
+   $lineBuffer = "";
+
+   rewind($fpointer);
+
+   $linesPerFile = $sourceLineCount / $numFiles;
+
+   if(is_dir($destDirectory)) {
+      shell_exec("rm -rf " . $destDirectory);
+   }
+
+   shell_exec("mkdir -p " . $destDirectory);
+   
+   for ($k=0; $k < $numFiles; $k++) {
+      $linesProcessedInFragment = 0; 
+      $fragmentFilePointer = fopen($destDirectory . "/" . $newFileName, "w");
+      
+      while ($linesProcessedInFragment < $linesPerFile && !feof($fpointer)) {
+         fwrite($fragmentFilePointer, fgets($fpointer));
+         $linesProcessedInFragment++; 
+      }
+      
+      fclose($fragmentFilePointer);
+      $newFileName = str_increment($newFileName);
+   }
+
+   if(!feof($fpointer)) {
+      $lastFilePointer = fopen(str_decrement($newFileName), "a");
+      while(!feof($fpointer)) {
+         fwrite($lastFilePointer, fgets($fpointer));
+      }
+      fclose($lastFilePointer);
+   } 
+}
+
+function countFile($sourceFile): int {
+   $fpointer = fopen($sourceFile, "r");
+   $lineCount = 0;
+   while(fgets($fpointer)) {
+      $lineCount++;
+   }
+   fclose($fpointer);
+   return $lineCount;
+}
+?>
